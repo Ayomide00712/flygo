40051df677e7f48a204eea384c41e5e8ebac430d
Ayomide00712 <usmanalkassim01@gmail.com>
2024-10-24 09:12:24 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+//** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+      current: 'currentColor',
+        'blue': '#3E9FD3',
+      },
+    },
+  },
+  plugins: [],
+}
+
